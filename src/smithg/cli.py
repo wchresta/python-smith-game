0d378503c9345a7088f4f6e0f2858ba4311a50bf
wchresta <34962284+wchresta@users.noreply.github.com>
2022-02-19 17:04:20 +0100

    Improve CLI, add verbosity and format flags

--- src/smithg/cli.py
+++ src/smithg/cli.py
@@ -1,10 +1,13 @@
+import argparse
 import importlib
 import pathlib
+import sys
+from typing import NamedTuple
+import logging
 
 import smithg.engine
 import smithg.agents
 
-import logging
 
 _logger = logging.getLogger(__name__)
 
@@ -16,8 +19,61 @@ def discover_agents(agents_package: str) -> None:
         importlib.import_module(module_name, package=None)
 
 
-def main():
-    logging.basicConfig(level=logging.INFO)
+class Result(NamedTuple):
+    name: str
+    score: int
+
+
+def output_text(results: list[Result]):
+    print("Simulation finished. Here are the results")
+    for name, score in results:
+        print(f"Agent {name:20} $ {score:8d}")
+
+
+def output_json(results: list[Result]):
+    import json
+
+    print(json.dumps(results))
+
+
+def output_csv(results: list[Result]):
+    import csv
+
+    writer = csv.writer(sys.stdout)
+    writer.writerow(["agent_name", "score"])
+    writer.writerows(results)
+
+
+_FORMATTERS = {
+    "text": output_text,
+    "json": output_json,
+    "csv": output_csv,
+}
+
+
+def parse_args(argv=None) -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="Run smith-game simulations.")
+    debug_level = parser.add_mutually_exclusive_group(required=False)
+    debug_level.add_argument("--log-level", default=logging.WARNING)
+    debug_level.add_argument("-v", "--verbose", action="count", default=0)
+
+    parser.add_argument(
+        "-f",
+        "--format",
+        help="Output format",
+        choices=tuple(_FORMATTERS.keys()),
+        default="text",
+    )
+
+    args = parser.parse_args(args=argv)
+    args.log_level -= 10 * args.verbose  # Every 10 reduces log-level by one
+
+    return args
+
+
+def main(argv=None):
+    args = parse_args(argv)
+    logging.basicConfig(level=args.log_level)
 
     agents_path = "player_agents"
     _logger.info("Loading agents from %s", agents_path)
@@ -27,9 +83,11 @@ def main():
     _logger.info("Running simulation...")
     agent_container = smithg.engine.simulate()
 
-    _logger.info("Simulation finished. Here are the results")
-    for cont in sorted(agent_container, key=lambda x: x.state.balance, reverse=True):
-        _logger.info("Agent %s, balance: %i", cont.agent_name, cont.state.balance)
+    results = [Result(cont.agent_name, cont.state.balance) for cont in agent_container]
+    results.sort(key=lambda r: r.score, reverse=True)
+
+    formatter = _FORMATTERS.get(args.format, output_text)
+    formatter(results)
 
 
 if __name__ == "__main__":
